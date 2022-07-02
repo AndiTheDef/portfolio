7a3f6ed227a031c44042c5bd1efc79d339ec1d8c
Andi Demi <107476532+AndiTheDef@users.noreply.github.com>
2022-07-02 20:32:58 +0200

    push deploy

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://andithedef.github.io/chat/",
+    origin: "https://andithedef.github.io/portfolio/",
     methods: ["GET", "POST"],
   },
 });

331043d7e57b4592fb08c6082af438d2f59af8c7
Andi Demi <107476532+AndiTheDef@users.noreply.github.com>
2022-07-02 20:51:07 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,34 +3,9 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 
-const { Server } = require("socket.io");
-
 app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "https://andithedef.github.io/portfolio/",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("socket", socket.id);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log("user with id ", socket.id + "joined room " + data);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("recieve_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected", socket.id);
-  });
-});
 
 server.listen(3001, () => {
   console.log("Running");
@@ -38,6 +13,6 @@ server.listen(3001, () => {
 
 app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
 
-app.get("/*", function(req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
